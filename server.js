194240a09df91e1f3fc369e8098d5af39a97d1b4
Степан Вадимович Сергеев <dead_antihrist@mail.ru>
2025-10-26 14:15:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,26 +7,21 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// Сохраняем маршруты персонажей
 let routes = {};
 
-// Тестовая проверка
 app.get('/', (req,res)=>res.send('EVE WH Map Backend running!'));
 
-// Получение маршрута
 app.get('/route/:characterId', (req,res)=>{
   const charId = req.params.characterId;
   res.json(routes[charId] || { steps: [] });
 });
 
-// Сохранение маршрута
 app.post('/route/:characterId', (req,res)=>{
   const charId = req.params.characterId;
   routes[charId] = req.body;
   res.json({ success: true });
 });
 
-// Удаление маршрута
 app.delete('/route/:characterId', (req,res)=>{
   const charId = req.params.characterId;
   delete routes[charId];
